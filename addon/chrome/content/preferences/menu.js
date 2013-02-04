eb03384967715d882303e63c9d108d17ace4f9d9
onemen <tabmix.onemen@gmail.com>
2013-02-04 15:42:03 +0200

    Update menu.xul and menu.js
    --HG--
    branch : preference

--- addon/chrome/content/preferences/menu.js
+++ addon/chrome/content/preferences/menu.js
@@ -5,24 +5,13 @@ var gMenuPane = {
     $("pinTab").label = gCommon.pinTabLabel;
 
     var browserWindow = Tabmix.getTopWin();
-//XXX we can drop it !!!!
     // check if bookmark item in tab context menu
     TM_Options.setItem("bmMenu", "hidden", !(browserWindow.document.getElementById("context_bookmarkAllTabs")));
 
-//    TM_Options.initBroadcasters("paneMenu", true);
     TM_Options.initUndoCloseBroadcaster();
     TM_Options.initSingleWindowBroadcaster();
-//XXX TODO if event pan not loaded yet $("selectTab") is null
-//  try to call $("selectTab").checked
-/*
-    var checked = $("selectTab") ? $("selectTab").checked || prevValue ?????
-*/
     this.setInverseLinkLabel();
-/*
-    var itam = $("selectTab");
-    var checked = itam ? itam.checked : Services.prefs.getBoolPref("browser.tabs.loadInBackground");
-    TM_Options.selectTab(checked);
-*/
+
     gCommon.setPaneWidth("paneMenu");
   },
 
