0e841029aba1c8e6df211821aa97e50ffdeef08a
onemen <tabmix.onemen@gmail.com>
2014-09-19 14:13:21 +0300

    AMO validation results warning: Please store complex scripts in .js files rather than inline script nodes

---
+++ addon/chrome/content/links/newTab.js
@@ -0,0 +1,35 @@
+"use strict";
+
+(function Tabmix_newTab() {
+  function updateTitle() {
+    if (!gGrid.cells)
+      return;
+    let win = Tabmix.getTopWin();
+    if (win)
+      win.TMP_Places._titlefrombookmark = Tabmix.prefs.getBoolPref("titlefrombookmark");
+    gGrid.cells.forEach(function (cell) {
+      let site = cell.site;
+      if (!site)
+        return;
+      let url = site.url;
+      let title = site.title || url;
+      title = win.TMP_Places.getTitleFromBookmark(url, title);
+      let tooltip = (title == url ? title : title + "\n" + url);
+      let link = site._querySelector(".newtab-link");
+      link.setAttribute("title", tooltip);
+      site._querySelector(".newtab-title").textContent = title;
+    });
+  }
+
+  let win = Tabmix.getTopWin()
+  if (win && win.Tabmix && win.Tabmix.initialization.onWindowOpen.initialized) {
+    Services.prefs.addObserver("extensions.tabmix.titlefrombookmark", updateTitle, false);
+    window.addEventListener("unload", function TMP_removeObserver(aEvent) {
+      aEvent.currentTarget.removeEventListener("unload", TMP_removeObserver, false);
+      Services.prefs.removeObserver("extensions.tabmix.titlefrombookmark", updateTitle);
+    }, false);
+    if (Tabmix.prefs.getBoolPref("titlefrombookmark"))
+      updateTitle();
+  }
+
+})()
