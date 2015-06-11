139056d59690072e9632fdeb61a7a259dd65ecb7
onemen <tabmix.onemen@gmail.com>
2015-06-11 15:17:49 +0300

    Fix typo in changeset 8abf33f8e0d4

--- addon/chrome/content/links/setup.js
+++ addon/chrome/content/links/setup.js
@@ -162,7 +162,7 @@ Tabmix.beforeBrowserInitOnLoad = function() {
         '      gBrowser.tabs[i].loadOnStartup = true;' +
         '    }' +
         '  }' +
-        '  if (uriToLoad == TabmixSvc.aboutNewtab || "tabmixdata" in window) {' +
+        '  if (uriToLoad == TabmixSvc.aboutBlank || "tabmixdata" in window) {' +
         '    gBrowser.selectedBrowser.stop();' +
         '  }\n' +
         '    $&';
@@ -261,7 +261,7 @@ Tabmix.beforeStartup = function TMP_beforeStartup(tabBrowser, aTabContainer) {
              return true;
           if (aBrowser.canGoForward || aBrowser.canGoBack)
              return false;
-          return aboutBlank ? aBrowser.currentURI.spec == TabmixSvc.aboutNewtab :
+          return aboutBlank ? aBrowser.currentURI.spec == TabmixSvc.aboutBlank :
                  Tabmix.isNewTabUrls(aBrowser.currentURI.spec);
        } catch (ex) {Tabmix.assert(ex); return true;}
     };
