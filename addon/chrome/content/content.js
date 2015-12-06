5cd46734ee67dd0f1d19f7825306465122c33c7a
onemen <tabmix.onemen@gmail.com>
2015-12-06 23:58:12 +0200

    Follow up bug 1218249 - check type of node before using xlink:href

--- addon/chrome/content/content.js
+++ addon/chrome/content/content.js
@@ -316,7 +316,9 @@ var TabmixClickEventHandler = {
     let href, baseURI;
     node = event.target;
     while (node && !href) {
-      if (node.nodeType == content.Node.ELEMENT_NODE) {
+      if (node.nodeType == content.Node.ELEMENT_NODE &&
+          (node.localName == "a" ||
+           node.namespaceURI == "http://www.w3.org/1998/Math/MathML")) {
         href = node.getAttributeNS("http://www.w3.org/1999/xlink", "href");
         if (href) {
           baseURI = node.ownerDocument.baseURIObject;
