547834d0cb9ff8dbb6039741c556e0896f942ff6
onemen <tabmix.onemen@gmail.com>
2018-05-22 19:00:00 +0300

    Follow up bug 1392352 - Part 2 - Translate the tabbrowser implementation into a JS class

--- addon/chrome/content/minit/tablib.js
+++ addon/chrome/content/minit/tablib.js
@@ -277,9 +277,9 @@ Tabmix.tablib = {
     // changed by bug #563337
     if (!Tabmix.extensions.tabGroupManager) {
       let aboutBlank = 'this.addTab("about:blank", {skipAnimation: true})';
-      let aboutNewtab = 'this.addTab(BROWSER_NEW_TAB_URL, {skipAnimation: true})';
-      let code = gBrowser._beginRemoveTab.toString().indexOf(aboutNewtab) > -1 ?
-        aboutNewtab : aboutBlank;
+      let aboutNewtab = /this\.addTab\(BROWSER_NEW_TAB_URL, {\s?skipAnimation: true\s?}\)/;
+      let code = gBrowser._beginRemoveTab.toString().indexOf(aboutBlank) > -1 ?
+        aboutBlank : aboutNewtab;
       Tabmix.changeCode(gBrowser, "gBrowser._beginRemoveTab")._replace(
         code, 'TMP_BrowserOpenTab(null, null, true)'
       ).toCode();
