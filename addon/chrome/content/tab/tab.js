onemen/TabMixPlus

commit 04a5ab20c404aabcdfce5a06a53219163293a647

Follow up bug 1692303 - Flip default value to browser.ctrlTab.recentlyUsedOrder to false

addon/chrome/content/tab/tab.js
@@ -1182,7 +1182,7 @@ gTMPprefObserver = {
     "browser.link.open_newwindow.override.external",
     "browser.link.open_newwindow.restriction",
     "browser.link.open_newwindow",
-    "browser.ctrlTab.recentlyUsedOrder"],
+    TabmixSvc.sortByRecentlyUsed],
 
   // removes the observer-object from service -- called when the window is no longer open
   removeObservers() {
@@ -1466,7 +1466,7 @@ gTMPprefObserver = {
       case "extensions.tabmix.newTabButton.position":
         this.changeNewTabButtonSide(Services.prefs.getIntPref(prefName));
         break;
-      case "browser.ctrlTab.recentlyUsedOrder":
+      case TabmixSvc.sortByRecentlyUsed:
       case "extensions.tabmix.lasttab.tabPreviews":
       case "extensions.tabmix.lasttab.respondToMouseInTabList":
       case "extensions.tabmix.lasttab.showTabList":
@@ -2192,18 +2192,6 @@ gTMPprefObserver = {
       Services.prefs.setBoolPref("browser.tabs.closeWindowWithLastTab", !Tabmix.prefs.getBoolPref("keepWindow"));
       Tabmix.prefs.clearUserPref("keepWindow");
     }
-    // 2008-09-23
-    if (Services.prefs.prefHasUserValue("browser.ctrlTab.mostRecentlyUsed")) {
-      Services.prefs.setBoolPref("browser.ctrlTab.recentlyUsedOrder",
-        Services.prefs.getBoolPref("browser.ctrlTab.mostRecentlyUsed"));
-      Services.prefs.clearUserPref("browser.ctrlTab.mostRecentlyUsed");
-    }
-    // 2008-09-28
-    if (Tabmix.prefs.prefHasUserValue("lasttab.handleCtrlTab")) {
-      Services.prefs.setBoolPref("browser.ctrlTab.recentlyUsedOrder",
-        Tabmix.prefs.getBoolPref("lasttab.handleCtrlTab"));
-      Tabmix.prefs.clearUserPref("lasttab.handleCtrlTab");
-    }
     // 2008-11-29
     if (Tabmix.prefs.prefHasUserValue("maxWidth")) {
       let val = getPrefByType("extensions.tabmix.maxWidth", 250, "IntPref");
@@ -2400,17 +2388,41 @@ gTMPprefObserver = {
     }
     // 2011-01-22 - verify sessionstore enabled
     Services.prefs.clearUserPref("browser.sessionstore.enabled");
-    // 2021-01-16
-    if (Services.prefs.prefHasUserValue("browser.ctrlTab.previews")) {
-      Services.prefs.setBoolPref("browser.ctrlTab.recentlyUsedOrder",
-        Services.prefs.getBoolPref("browser.ctrlTab.previews"));
-      Services.prefs.clearUserPref("browser.ctrlTab.previews");
-    }
     // 2021-01-22
     if (Services.prefs.prefHasUserValue("extensions.tabmix.hideAllTabsButton")) {
       Services.prefs.setBoolPref("browser.tabs.tabmanager.enabled",
         Services.prefs.getBoolPref("extensions.tabmix.hideAllTabsButton"));
       Services.prefs.clearUserPref("extensions.tabmix.hideAllTabsButton");
+    }
+    // 2021-04-05
+    // Bug 1692303 - Migrate old ctrlTab pref to new ctrlTab pref
+    function migrateCtrlTab(oldPrefName) {
+      if (Services.prefs.prefHasUserValue(oldPrefName)) {
+        let newPrefValue = Services.prefs.getBoolPref(oldPrefName);
+        if (Tabmix.isVersion(890)) {
+          if (Services.prefs.getBoolPref("browser.engagement.ctrlTab.has-used")) {
+            Services.prefs.setBoolPref(
+              "browser.ctrlTab.sortByRecentlyUsed",
+              newPrefValue
+            );
+          } else {
+            Services.prefs.setBoolPref(
+              "browser.ctrlTab.sortByRecentlyUsed",
+              false
+            );
+          }
+        } else {
+          Services.prefs.setBoolPref("browser.ctrlTab.recentlyUsedOrder",
+            newPrefValue);
+        }
+        Services.prefs.clearUserPref(oldPrefName);
+      }
+    }
+    migrateCtrlTab("browser.ctrlTab.mostRecentlyUsed");
+    migrateCtrlTab("extensions.tabmix.lasttab.handleCtrlTab");
+    migrateCtrlTab("browser.ctrlTab.previews");
+    if (Tabmix.isVersion(890)) {
+      migrateCtrlTab("browser.ctrlTab.recentlyUsedOrder");
     }
 
     let getVersion = function _getVersion(currentVersion, shouldAutoUpdate) {

addon/chrome/content/flst/lasttab.js
@@ -352,7 +352,7 @@ var TMP_LastTab = {
 
   ReadPreferences() {
     // when Build-in tabPreviews is on we disable our own function
-    var mostRecentlyUsed = Services.prefs.getBoolPref("browser.ctrlTab.recentlyUsedOrder");
+    var mostRecentlyUsed = Services.prefs.getBoolPref(TabmixSvc.sortByRecentlyUsed);
     var tabPreviews = document.getElementById("ctrlTab-panel") && "ctrlTab" in window;
     if (tabPreviews) {
       var tabPreviewsCurrentStatus = Boolean(ctrlTab._recentlyUsedTabs);

addon/chrome/content/preferences/events.js
@@ -46,6 +46,18 @@ var gEventsPane = {
     gPrefWindow.removeChild("pref_disableTabsAnimate");
     gPrefWindow.removeChild("disableTabsAnimate");
 
+    if (!Tabmix.isVersion(890)) {
+      gPrefWindow.removeChild("pref_ctrltab");
+      const preferences = document.getElementById("paneEvents").querySelector("preferences");
+      const NS_XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+      const preference = document.createElementNS(NS_XUL, "preference");
+      preference.id = "pref_ctrltab";
+      preference.setAttribute("name", "browser.ctrlTab.recentlyUsedOrder");
+      preference.setAttribute("type", "bool");
+      preference.setAttribute("onchange", "gEventsPane.disableShowTabList();");
+      preferences.appendChild(preference);
+    }
+
     $("restoreOnDemand").setAttribute("type", "number");
 
     this.alignTabOpeningBoxes();

addon/chrome/content/preferences/events.xhtml
@@ -99,7 +99,7 @@
                   name="extensions.tabmix.undoCloseButton.menuonly"                           type="bool"/>
       <preference id="pref_keepMenuOpen"
                   name="extensions.tabmix.undoClose.keepMenuOpen"                             type="bool"/>
-      <preference id="pref_ctrltab"             name="browser.ctrlTab.recentlyUsedOrder"      type="bool"
+      <preference id="pref_ctrltab"             name="browser.ctrlTab.sortByRecentlyUsed"     type="bool"
                   onchange="gEventsPane.disableShowTabList();"/>
       <preference id="pref_ctrltab.tabPreviews" name="extensions.tabmix.lasttab.tabPreviews"  type="bool"
                   onchange="gEventsPane.disableShowTabList();"/>

addon/chrome/content/preferences/preferences.js
@@ -353,7 +353,7 @@ var sessionPrefs = ["browser.sessionstore.resume_from_crash",
 XPCOMUtils.defineLazyGetter(window, "gPreferenceList", () => {
   // other settings not in extensions.tabmix. branch that we save
   let otherPrefs = [
-    "browser.allTabs.previews", "browser.ctrlTab.recentlyUsedOrder",
+    "browser.allTabs.previews", TabmixSvc.sortByRecentlyUsed,
     "browser.link.open_newwindow", "browser.link.open_newwindow.override.external",
     "browser.link.open_newwindow.restriction", TabmixSvc.newtabUrl,
     "browser.search.context.loadInBackground", "browser.search.openintab",

addon/modules/TabmixSvc.jsm
@@ -227,6 +227,9 @@ this.TabmixSvc = {
         return;
       this._initialized = true;
 
+      TabmixSvc.sortByRecentlyUsed = isVersion(890) ?
+        "browser.ctrlTab.sortByRecentlyUsed" : "browser.ctrlTab.recentlyUsedOrder";
+
       try {
         // replace old Settings.
         // we must call this before any other tabmix function

commit 6357af0365449aaa41bc7180476f5eacd055da0f

Our prefs observer (tab.js) observing ALL preferences regressed by commit 04a5ab20c404aabcdfce5a06a53219163293a647

addon/chrome/content/tab/tab.js
@@ -1101,7 +1101,7 @@ gTMPprefObserver = {
         this.OBSERVING.push(pref);
     };
     addObserver("layout.css.devPixelsPerPx", TabmixSvc.australis);
-    addObserver("browser.tabs.tabmanager.enabled", true);
+    addObserver(TabmixSvc.sortByRecentlyUsed, true);
     addObserver("browser.proton.enabled", Tabmix.isVersion(890));
 
     try {
@@ -1125,7 +1125,8 @@ gTMPprefObserver = {
     "browser.link.open_newwindow.override.external",
     "browser.link.open_newwindow.restriction",
     "browser.link.open_newwindow",
-    TabmixSvc.sortByRecentlyUsed],
+    "browser.tabs.tabmanager.enabled",
+  ],
 
   // removes the observer-object from service -- called when the window is no longer open
   removeObservers() {
@@ -1139,16 +1140,21 @@ gTMPprefObserver = {
    * subject: [wrapped nsISupports :: nsIPrefBranch], nsIPrefBranch Internal
    * topic: "changed"
    */
+  prefsValues: {},
   observe: function TMP_pref_observer(subject, topic, prefName) {
-    if (this.preventUpdate)
+    if (this.preventUpdate ||
+      this.prefsValues[prefName] === TabmixSvc.prefs.get(prefName)) {
       return;
+    }
+    const prefValue = TabmixSvc.prefs.get(prefName);
+    this.prefsValues[prefName] = prefValue;
+
     // if we don't have a valid window (closed)
     if (!(typeof (document) == 'object' && document)) {
       this.removeObservers(); // remove the observer..
       return; // ..and don't continue
     }
 
-    var prefValue, value;
     switch (prefName) {
       case "extensions.tabmix.titlefrombookmark":
         TMP_Places.onPreferenceChanged(Services.prefs.getBoolPref(prefName));
@@ -1282,8 +1288,7 @@ gTMPprefObserver = {
         gBrowser.tabContainer._updateCloseButtons();
         break;
       case "browser.tabs.closeButtons":
-        value = Services.prefs.getIntPref(prefName);
-        switch (value) {
+        switch (prefValue) {
           case 0: // Display a close button on the active tab only
             Tabmix.prefs.setIntPref("tabs.closeButtons", 3);
             break;
@@ -1298,15 +1303,14 @@ gTMPprefObserver = {
             return;
         }
         // show/hide close button on tabs
-        Tabmix.prefs.setBoolPref("tabs.closeButtons.enable", value < 2);
+        Tabmix.prefs.setBoolPref("tabs.closeButtons.enable", prefValue < 2);
         // show/hide close button on the tabbar
-        Tabmix.prefs.setBoolPref("hideTabBarButton", value != 3);
+        Tabmix.prefs.setBoolPref("hideTabBarButton", prefValue != 3);
         break;
       case "extensions.tabmix.tabs.closeButtons":
-        value = Services.prefs.getIntPref(prefName);
-        if (value < 1 || value > 5) {
+        if (prefValue < 1 || prefValue > 5) {
           Services.prefs.setIntPref(prefName, 1);
-        } else if (value == 5 && TabmixTabbar.widthFitTitle) {
+        } else if (prefValue == 5 && TabmixTabbar.widthFitTitle) {
           Services.prefs.setIntPref(prefName, 1);
         } else {
           gBrowser.tabContainer.mCloseButtons = Services.prefs.getIntPref(prefName);
@@ -1319,7 +1323,6 @@ gTMPprefObserver = {
         break;
       }
       case "extensions.tabmix.tabs.closeButtons.enable":
-        prefValue = Services.prefs.getBoolPref(prefName);
         Tabmix.tabsUtils.closeButtonsEnabled = prefValue;
         gBrowser.tabContainer.arrowScrollbox.offsetRatio = prefValue ? 0.70 : 0.50;
         gBrowser.tabContainer._updateCloseButtons();
@@ -1344,7 +1347,6 @@ gTMPprefObserver = {
         break;
       case "browser.sessionstore.max_tabs_undo": {
         // Firefox's sessionStore maintain the right amount
-        prefValue = Services.prefs.getIntPref(prefName);
         if (Tabmix.prefs.getBoolPref("undoClose") != (prefValue > 0))
           Tabmix.prefs.setBoolPref("undoClose", prefValue > 0);
         break;