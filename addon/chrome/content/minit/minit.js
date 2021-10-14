onemen/TabMixPlus

commit e670751fc4b85102807e720c196323917ef08d39

fix: open tabs from: Address bar fails (closes #89)

addon/chrome/content/minit/minit.js
@@ -901,6 +901,15 @@ Tabmix.navToolbox = {
     if (!this.urlBarInitialized) {
       Tabmix.originalFunctions.gURLBar_handleCommand = gURLBar.handleCommand;
       gURLBar.handleCommand = this.handleCommand.bind(gURLBar);
+
+      Tabmix.originalFunctions.gURLBar__whereToOpen = gURLBar._whereToOpen;
+      gURLBar._whereToOpen = function(event) {
+        if (event?.__tabmix__whereToOpen) {
+          return event.__tabmix__whereToOpen;
+        }
+        return Tabmix.originalFunctions.gURLBar__whereToOpen.apply(this, arguments);
+      };
+
       gURLBar.view._rows.addEventListener("mouseup", event => {
         this.on_mouseup(event);
       }, {capture: true});
@@ -908,7 +917,7 @@ Tabmix.navToolbox = {
     }
   },
 
-  handleCommand(event, openUILinkWhere, openUILinkParams = {}) {
+  handleCommand(event, openUILinkWhere) {
     let prevTab, prevTabPos;
     let element = this.view.selectedElement;
     let result = this.view.getResultFromElement(element);
@@ -918,6 +927,7 @@ Tabmix.navToolbox = {
       prevTabPos = prevTab._tPos;
     }
 
+    // openUILinkWhere is not used since Firefox 83
     if (!openUILinkWhere) {
       let isMouseEvent = event instanceof MouseEvent;
       let altEnter = !isMouseEvent && event &&
@@ -936,11 +946,13 @@ Tabmix.navToolbox = {
         else if (!isMouseEvent && !loadNewTab && /^tab/.test(where))
           where = "current";
       }
-      openUILinkWhere = where;
+      if (Tabmix.prefs.getBoolPref("loadUrlInBackground") && where === "tab") {
+        where = "tabshifted";
+      }
+      event.__tabmix__whereToOpen = where;
     }
 
-    openUILinkParams.inBackground = Tabmix.prefs.getBoolPref("loadUrlInBackground");
-    Tabmix.originalFunctions.gURLBar_handleCommand.call(gURLBar, event, openUILinkWhere, openUILinkParams);
+    Tabmix.originalFunctions.gURLBar_handleCommand.apply(this, arguments);
 
     // move the tab that was switched to after the previously selected tab
     if (typeof prevTabPos == "number") {

commit 29d71e6073006a28e75be5a376ea218932890598

fix: regression from e670751 (closes #89)

addon/chrome/content/minit/minit.js
@@ -907,6 +907,11 @@ Tabmix.navToolbox = {
         if (event?.__tabmix__whereToOpen) {
           return event.__tabmix__whereToOpen;
         }
+        if (gBrowser.selectedBrowser.__tabmix__whereToOpen) {
+          const where = gBrowser.selectedBrowser.__tabmix__whereToOpen;
+          delete gBrowser.selectedBrowser.__tabmix__whereToOpen;
+          return where;
+        }
         return Tabmix.originalFunctions.gURLBar__whereToOpen.apply(this, arguments);
       };
 
@@ -949,10 +954,23 @@ Tabmix.navToolbox = {
       if (Tabmix.prefs.getBoolPref("loadUrlInBackground") && where === "tab") {
         where = "tabshifted";
       }
-      event.__tabmix__whereToOpen = where;
+      if (event) {
+        event.__tabmix__whereToOpen = where;
+      } else if (
+        where.startsWith("tab") &&
+        // when user clicked paste & go with current url we let Firefox
+        // reload the same url
+        this.untrimmedValue !== gBrowser.selectedBrowser.currentURI.spec
+      ) {
+        gBrowser.selectedBrowser.__tabmix__whereToOpen = where;
+      }
     }
 
     Tabmix.originalFunctions.gURLBar_handleCommand.apply(this, arguments);
+
+    if (gBrowser.selectedBrowser.__tabmix__whereToOpen) {
+      delete gBrowser.selectedBrowser.__tabmix__whereToOpen;
+    }
 
     // move the tab that was switched to after the previously selected tab
     if (typeof prevTabPos == "number") {

addon/chrome/content/minit/tablib.js
@@ -40,6 +40,14 @@ Tabmix.tablib = {
       }
       return null;
     };
+
+    for (const tab of gBrowser.tabs) {
+      const browser = tab.linkedBrowser;
+      browser.tabmix_allowLoad = !TabmixTabbar.lockallTabs;
+      if (tab.linkedPanel) {
+        browser.loadURI = _loadURI.bind(null, browser);
+      }
+    }
   },
 
   _loadURI(browser, uri, params) {