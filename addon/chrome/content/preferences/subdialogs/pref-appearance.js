onemen/TabMixPlus

commit 966bfbc3679f40fff4be37dd6440b7e8daeb36c4

fix: content cutoff for 'File Type Editor' and 'Customize Styles' dialogs

addon/chrome/content/preferences/subdialogs/pref-appearance.xhtml
@@ -4,25 +4,25 @@
 <?xml-stylesheet href="chrome://tabmixplus/content/preferences/subdialogs/pref-appearance.css" type="text/css"?>
 <?xml-stylesheet href="chrome://tabmixplus/skin/pref-appearance.css"?>
 
-<!DOCTYPE dialog [
+<!DOCTYPE window [
 <!ENTITY % dialogDTD SYSTEM "chrome://tabmixplus/locale/pref-tabmix.dtd" >
 %dialogDTD;
 <!ENTITY % appearanceDTD SYSTEM "chrome://tabmixplus/locale/pref-appearance.dtd" >
 %appearanceDTD;
 ]>
 
-<dialog id="pref-TMP-styles"
+<window id="pref-TMP-styles"
         class="system-font-size"
         windowtype="mozilla:tabmixopt-appearance"
         xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
         xmlns:html="http://www.w3.org/1999/xhtml"
-        buttons="accept,cancel,extra1,extra2"
         title="&styles.label;"
         persist="screenX screenY hide-RGB"
-        onload="tabstyles.init();"
         ondialogcancel="tabstyles.cancel();"
         ondialogaccept="tabstyles.save();"
         ondialogextra2="tabstyles.toggleRGB_visibility();">
+<dialog buttons="accept,cancel,extra1,extra2"
+        onload="tabstyles.init();">
 
    <script type="text/javascript"><![CDATA[
     /* global tabstyles */
@@ -62,3 +62,4 @@
      <button dlgtype="extra1" class="dialog-button" hidden="true" icon="help"/>
    </hbox>
 </dialog>
+</window>

addon/chrome/content/preferences/subdialogs/pref-filetype.xhtml
@@ -3,23 +3,22 @@
 <?xml-stylesheet href="chrome://global/skin/" type="text/css"?>
 <?xml-stylesheet href="chrome://tabmixplus/skin/preferences.css"?>
 
-<!DOCTYPE dialog [
+<!DOCTYPE window [
 <!ENTITY % dialogDTD SYSTEM "chrome://tabmixplus/locale/pref-tabmix.dtd" >
 %dialogDTD;
 <!ENTITY % filetypeDTD SYSTEM "chrome://tabmixplus/locale/pref-filetype.dtd" >
 %filetypeDTD;
 ]>
 
-<dialog id="pref-TMP-filetype"
+<window id="pref-TMP-filetype"
         class="system-font-size"
         windowtype="mozilla:tabmixopt-filetype"
         xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
         xmlns:html="http://www.w3.org/1999/xhtml"
-        buttons="accept,cancel,extra1"
         title="&filetype.options;"
         onload="Init()"
         persist="screenX screenY">
-
+<dialog buttons="accept,cancel,extra1">
    <script type="text/javascript"><![CDATA[
      /* global Save */
      this.addEventListener("dialogaccept", () => Save());
@@ -50,3 +49,4 @@
      <button dlgtype="extra1" class="dialog-button" hidden="true" icon="help"/>
    </hbox>
 </dialog>
+</window>

commit 46be8f05c77fc9b762b63fcccf05d3ed3fc2c830

fix: regression from commit 966bfbc, 'Customize Styles' dialogs 'Hide RGB' stop working

addon/chrome/content/preferences/subdialogs/pref-appearance.js
@@ -10,10 +10,12 @@ const tabstyles = {
     $("stylestabs").selectedIndex = Tabmix.prefs.prefHasUserValue(this.pref) ?
       Tabmix.prefs.getIntPref(this.pref) : 0;
 
-    if (!window.opener && !Tabmix.getTopWin())
-      document.documentElement.getButton("help").disabled = true;
+    const dialog = document.querySelector("dialog");
+    if (!window.opener && !Tabmix.getTopWin()) {
+      dialog.getButton("extra1").disabled = true;
+    }
 
-    const extra = document.documentElement.getButton("extra2");
+    const extra = dialog.getButton("extra2");
     extra.label = $("hide-RGB").value;
     extra.classList.add("text-link");
 
@@ -36,18 +38,19 @@ const tabstyles = {
 
   openHelp() {
     const win = window.opener || Tabmix.getTopWin();
-    if (win)
+    if (win) {
       win.openHelp("display-tab#customize_styles");
-    else
-      document.documentElement.getButton("help").disabled = true;
+    } else {
+      this.dialog.getButton("extra1").disabled = true;
+    }
   },
 
   toggleRGB_visibility() {
-    const doc = document.documentElement;
-    const extra = doc.getButton("extra2");
+    const dialog = document.querySelector("dialog");
+    const extra = dialog.getButton("extra2");
     const item = $("hide-RGB");
-    const wasShow = doc.getAttribute("hide-RGB") != "true";
+    const wasShow = dialog.getAttribute("hide-RGB") != "true";
     extra.label = item.value = item.getAttribute(wasShow ? 'show' : 'hide');
-    doc.setAttribute("hide-RGB", wasShow);
+    dialog.setAttribute("hide-RGB", wasShow);
   }
 };

addon/chrome/content/preferences/subdialogs/pref-appearance.xhtml
@@ -18,11 +18,11 @@
         xmlns:html="http://www.w3.org/1999/xhtml"
         title="&styles.label;"
         persist="screenX screenY hide-RGB"
+        onload="tabstyles.init();"
         ondialogcancel="tabstyles.cancel();"
         ondialogaccept="tabstyles.save();"
         ondialogextra2="tabstyles.toggleRGB_visibility();">
-<dialog buttons="accept,cancel,extra1,extra2"
-        onload="tabstyles.init();">
+<dialog buttons="accept,cancel,extra1,extra2">
 
    <script type="text/javascript"><![CDATA[
     /* global tabstyles */