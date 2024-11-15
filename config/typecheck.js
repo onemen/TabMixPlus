5921aaa536e584c0109bac4e53592fbdbd16fd9b
onemen <tabmix.onemen@gmail.com>
2024-11-15 13:53:54 +0200

    chore: faster typecheck

--- config/typecheck.js
+++ config/typecheck.js
@@ -1,52 +1,76 @@
 import child_process from "node:child_process";
 import fs from "node:fs";
 
-const inputFile = "./tsc.local.txt";
-const outputFile = "./tsc.clean.local.txt";
+const outputFile = "./tsc.local.txt";
 
-function filterLines(input, output) {
-  fs.readFile(input, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return;
-    }
+const typesFolder = 'types';
+const LAST_RUN_FILE = "config/last_run.local.json";
 
-    const lines = [];
-    for (const line of data.split("\n")) {
-      if (line.startsWith(" ")) {
-        lines[lines.length - 1] = lines.at(-1) + "\n" + line;
-      } else {
-        lines.push(line);
-      }
-    }
-    const prefixes = ["addon", "config", "types"];
-    const filteredLines = lines.filter(line => prefixes.some(prefix => line.startsWith(prefix)));
-    const cleanData = filteredLines.length ?
-      filteredLines.join("\n") :
-      "No errors found!";
-
-    try {
-      fs.writeFileSync(output, cleanData, "utf8");
-      console.log(`File ${outputFile} written successfully!`);
-    } catch (error) {
-      console.error("Error writing file:", error);
-    }
-  });
-}
-function main() {
-  // clear previous output
-  fs.writeFileSync(outputFile, "", "utf8");
-
-  // Execute TypeScript compilation
+function getLastSavedCommit() {
   try {
-    child_process.execSync(`tsc -b -f> ${inputFile}`);
-  } catch {}
+    const data = fs.readFileSync(LAST_RUN_FILE, "utf8");
+    return JSON.parse(data);
+  } catch {
+    return {commit: 0};
+  }
+}
 
-  // Call your filterLines function
-  filterLines(inputFile, outputFile);
+function saveLastCommit(commit) {
+  fs.writeFileSync(LAST_RUN_FILE, JSON.stringify({commit}));
+}
 
-  // Delete the input file
-  fs.unlinkSync(inputFile);
+function getLastCommitSha() {
+  return child_process.execSync(`git log -1 --format=%H ${typesFolder}`).toString().trim();
+}
+
+function getModifiedFiles() {
+  return child_process.execSync(`git status --porcelain ${typesFolder}`).toString().trim();
+}
+
+const colors = {};
+const colorsCode = {
+  green: '\x1b[32m',
+  red: '\x1b[31m',
+  reset: '\x1b[0m'
+};
+
+Object.keys(colorsCode).forEach(name => {
+  const tagFunction = (strings, ...values) => {
+    return `${colorsCode[name]}${strings.reduce((acc, str, i) => acc + str + (values[i] || ''), '')}${colorsCode.reset}`;
+  };
+  colors[name] = tagFunction;
+});
+
+function runTsc(build) {
+  const command = build ? `tsc --build` : `tsc --build --incremental`;
+  let output;
+  try {
+    output = child_process.execSync(command, {encoding: 'utf8'});
+  } catch (error) {
+    output = error.stdout;
+  }
+  if (output) {
+    fs.writeFileSync(outputFile, output);
+    const errorCount = (output.match(/error TS\d+/g) || []).length;
+    const errorsString = errorCount > 1 ? "errors" : "error";
+    console.log(colors.red`Typecheck completed with ${errorCount} ${errorsString}.`, `See ${outputFile} for details.`);
+  } else {
+    fs.writeFileSync(outputFile, "No errors found!", "utf8");
+    console.log(colors.green`Typecheck completed successfully. No errors found!`);
+  }
+}
+
+function main() {
+  const lastCommitSha = getLastCommitSha();
+  const lastSavedSha = getLastSavedCommit().commit;
+  const modifiedFiles = getModifiedFiles();
+
+  if (lastCommitSha !== lastSavedSha || modifiedFiles) {
+    saveLastCommit(lastCommitSha);
+    runTsc(true);
+  } else {
+    runTsc(false);
+  }
 }
 
 main();
