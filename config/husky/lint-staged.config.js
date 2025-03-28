3042c932ae4842d36e726d2119ef28e227b135d9
onemen <tabmix.onemen@gmail.com>
2025-03-28 08:08:19 +0300

    chore: lint css files with stylelint

--- config/husky/lint-staged.config.js
+++ config/husky/lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   "*.{js,jsm,xhtml}":
     "eslint --format stylish --fix --cache --cache-location config/.eslintcache --cache-strategy content --no-warn-ignored",
+  "*.css": 'stylelint --fix --config config/.stylelintrc.json "addon/**/*.css"',
 };
