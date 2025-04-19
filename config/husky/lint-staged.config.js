a97936afec82ff887a63553a363ea294de84aaf7
onemen <tabmix.onemen@gmail.com>
2025-04-19 16:53:06 +0300

    chore: optimize lint-staged config

--- config/husky/lint-staged.config.js
+++ config/husky/lint-staged.config.js
@@ -1,5 +1,17 @@
 export default {
-  "*.{js,jsm,xhtml}":
-    "eslint --format stylish --fix --cache --cache-location config/.eslintcache --cache-strategy content --no-warn-ignored",
-  "*.css": 'stylelint --fix --config config/.stylelintrc.json "addon/**/*.css"',
+  // Root-level JS and JSON files
+  "*.{js,json}": [
+    "eslint --fix --format stylish --fix --cache --cache-location config/.eslintcache --cache-strategy content --no-warn-ignored",
+  ],
+
+  // JS/JSX/XHTML in addon/ and config/
+  "addon/**/*.{js,jsm,xhtml}": [
+    "eslint --fix --format stylish --fix --cache --cache-location config/.eslintcache --cache-strategy content --no-warn-ignored",
+  ],
+  "config/**/*.{js,jsm,xhtml}": [
+    "eslint --fix --format stylish --fix --cache --cache-location config/.eslintcache --cache-strategy content --no-warn-ignored",
+  ],
+
+  // CSS in addon/
+  "addon/**/*.css": ["stylelint --fix --config config/.stylelintrc.json"],
 };
