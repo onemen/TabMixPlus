onemen/TabMixPlus

commit d4c87de4fafd868ad2622a63cf6453777518e394

fix: moving collapsed group with active tab (closes #516)

addon/chrome/content/tab/tab.js
@@ -2311,7 +2311,8 @@ window.gTMPprefObserver = {
       // Bug 1949401 (Firefox 142) - Active Tabs should remain active for collapsed Tab groups
       let groupSelector =
         Tabmix.isVersion(1420) ?
-          `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected])`
+          `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected]):not(
+          :root:has(#tabbrowser-tabs:not([tabmix-multibar])) tab-group[movingtabgroup] > *)`
         : `tab-group:not([collapsed]) > ${tabSelector}`;
       newRule = `#tabbrowser-arrowscrollbox > ${groupSelector},
         #tabbrowser-arrowscrollbox > ${tabSelector} {
@@ -2334,7 +2335,8 @@ window.gTMPprefObserver = {
     // Bug 1949401 (Firefox 142) - Active Tabs should remain active for collapsed Tab groups
     let groupSelector =
       Tabmix.isVersion(1420) ?
-        `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected])`
+        `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected]):not(
+         :root:has(#tabbrowser-tabs:not([tabmix-multibar])) tab-group[movingtabgroup] > *)`
       : `tab-group:not([collapsed]) > ${tabSelector}`;
     let newRule = `#tabbrowser-arrowscrollbox:not([orient="vertical"]) > ${groupSelector},
       #tabbrowser-arrowscrollbox:not([orient="vertical"]) > ${tabSelector} {

commit f446fbd32d9af9fd8ac8747f9777a4c27f361e6e

fix: tab width fills rows on overflow does not work as expected (closes #526)
regressed by  d4c87de4fafd868ad2622a63cf6453777518e394

addon/chrome/content/tab/tab.js
@@ -2346,11 +2346,16 @@ window.gTMPprefObserver = {
     // Bug 1949401 (Firefox 142) - Active Tabs should remain active for collapsed Tab groups
     let groupSelector =
       Tabmix.isVersion(1420) ?
-        `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected]):not(
-         :root:has(#tabbrowser-tabs:not([tabmix-multibar])) tab-group[movingtabgroup] > *)`
+        `tab-group > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected])`
       : `tab-group:not([collapsed]) > ${tabSelector}`;
 
-    let newRule = `#tabbrowser-arrowscrollbox:not([orient="vertical"]) > ${groupSelector},
+    let combinedGroupSelector =
+      Tabmix.isVersion(1420) ?
+        `#tabbrowser-tabs[orient="horizontal"][tabmix-multibar] ${groupSelector},
+         #tabbrowser-tabs[orient="horizontal"]:not([tabmix-multibar]) tab-group:not([movingtabgroup]) > ${tabSelector}:is(tab-group:not([collapsed]) > *, [visuallyselected])`
+      : `#tabbrowser-arrowscrollbox:not([orient="vertical"]) > ${groupSelector}`;
+
+    let newRule = `${combinedGroupSelector},
       #tabbrowser-tabs[orient="horizontal"] tab-split-view-wrapper > ${tabSelector},
       #tabbrowser-arrowscrollbox:not([orient="vertical"]) > ${tabSelector} {
         min-width: calc(${_min}px + var(--tab-min-width-extra-icons, 0px) + var(--tab-min-width-uidensity, 0px)) !important;